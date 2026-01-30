95becc01bb848f3df1ee458177b1aa45f00d5cff
sudiptaMukherjee2001 <frontendDeveloperabbs@gmail.com>
2026-01-30 14:27:27 +0530

    Resume updated

--- src/component/IntroSection.jsx
+++ src/component/IntroSection.jsx
@@ -6,7 +6,7 @@ import { Box, Grid } from '@mui/material';
 import Link from 'next/link';
 
 const IntroSection = ({onProjectClick}) => {
-    const myResume = "/Sudipta-Resume.pdf";
+    const myResume = "/Sudipta_Cloud_Resume.pdf";
 
     return (
         <Grid container
